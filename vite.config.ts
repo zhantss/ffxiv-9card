169b630b44a22d2794960c6ccf9529edbb0ea11f
zhantss <zhantss@hotmail.com>
2022-06-15 19:39:29 +0800

    v0.0.6
    部分数据修复
    预先加入拆分及压缩打包插件

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,43 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
+import rollupPluginVisualizer from 'rollup-plugin-visualizer';
+// import viteCompression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [vue()],
+  build: {
+    // rollupOptions: {
+    //   output: {
+    //     manualChunks(id: string) {
+    //       if (id.includes('node_modules')) {
+    //         if (id.includes('vue')) {
+    //           return 'vue';
+    //         }
+    //         if (id.includes('naive-ui') || id.includes('vfonts')) {
+    //           return 'ui';
+    //         }
+    //         return 'vendor';
+    //       }
+    //       return 'index';
+    //     },
+    //   },
+    // },
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
+  },
+  plugins: [
+    vue(),
+    rollupPluginVisualizer({
+      open: false,
+    }),
+    // viteCompression(),
+  ],
   server: {
     port: 23001,
   },
